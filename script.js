8d7d9f67296c6e033840965d376dd681226cb30a
Sam McCauley <sammccauley34@gmail.com>
2023-06-10 14:05:00 -0500

    Added addEmployeeInfo and deleteEmployeeInfo functions to JS

--- script.js
+++ script.js
@@ -1,14 +1,81 @@
-console.log('js loaded!');
+console.log('JS loaded!');
 
 // ## Assignment
 
-// The application should have an input form that collects _employee 
+// ✅ The application should have an input form that collects _employee 
 // first name, last name, ID number, job title, annual salary_.
-// A 'Submit' button should collect the form information, store the information 
+
+// ✅ A 'Submit' button should collect the form information, store the information 
 // to calculate monthly costs, append information to the DOM and clear 
-// the input fields. Using the stored information, calculate monthly costs 
+// the input fields.
+
+// Using the stored information, calculate monthly costs 
 // and append this to the to DOM. If the total monthly cost exceeds $20,000, 
 // add a red background color to the total monthly cost.
 
+// ✅ Create a delete button that removes an employee from the DOM. For Base mode, 
+// it does **not** need to remove that Employee's salary from the reported total.
+
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('JQuery loaded!');
+
+
+    $('#submit-button').on('click', addEmployeeInfo)
+
+    $('#table-body').on('click', '.delete-button', deleteEmployeeInfo)
+}
+
+// A 'Submit' button should collect the form information, store the information 
+// to calculate monthly costs, append information to the DOM and clear 
+// the input fields.
+function addEmployeeInfo(event){
+    console.log('in addEmployeeInfo')
+    event.preventDefault();
+
+    //collect and store form info
+    let firstNameInputValue = $('#first-name-input').val();
+    console.log('firstNameInputValue is:', firstNameInputValue);
+
+    let lastNameInputValue = $('#last-name-input').val();
+    console.log('lastNameInputValue is:', lastNameInputValue);
+
+    let idInputValue = $('#id-input').val();
+    console.log('idInputValue is:', idInputValue);
+
+    let titleInputValue = $('#title-input').val();
+    console.log('titleInputValue is:', titleInputValue);
+
+    let annualSalaryInputValue = $('#annual-salary-input').val();
+    console.log('annualSalaryInputValue is:', annualSalaryInputValue);
+
+    // Append employee info into table
+    $('#table-body').append(`
+        <tr>
+            <td>${firstNameInputValue}</td>
+            <td>${lastNameInputValue}</td>
+            <td>${idInputValue}</td>
+            <td>${titleInputValue}</td>
+            <td>${annualSalaryInputValue}</td>
+            <td><button class="delete-button">delete</button></td>
+        </tr>
+    `)
+
+    // clear form
+    $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-input').val('');
+    $('#title-input').val('');
+    $('#annual-salary-input').val('');
+}
+
+
 // Create a delete button that removes an employee from the DOM. For Base mode, 
 // it does **not** need to remove that Employee's salary from the reported total.
+function deleteEmployeeInfo(){
+    console.log('in deleteEmployeeInfo');
+
+    $(this).parent().parent().remove();
+}
