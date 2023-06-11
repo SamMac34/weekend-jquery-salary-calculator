f8b0410730d8b9817216f8cb3f6b213832ddabf0
Sam McCauley <sammccauley34@gmail.com>
2023-06-11 00:30:59 -0500

    Working on formatting monthly salary info and conditional statements if salary input value is NaN

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ console.log('JS loaded!');
 // to calculate monthly costs, append information to the DOM and clear 
 // the input fields.
 
-// Using the stored information, calculate monthly costs 
+// ✅ Using the stored information, calculate monthly costs 
 // and append this to the to DOM. If the total monthly cost exceeds $20,000, 
 // add a red background color to the total monthly cost.
 
 // ✅ Create a delete button that removes an employee from the DOM. For Base mode, 
 // it does **not** need to remove that Employee's salary from the reported total.
+
+// STRETCH GOALS
+// Once the employee is deleted, update the _Total Monthly Cost_ section on the page to reflect
+// the employee's removal. _HINT:_ You will need to figure out which employee was removed in order
+//  to subtract their salary from the total. Consider using `.text()` as a getter, or look into
+//  jQuery's `.data()` function. This is tricky! 
 
 
 $(document).ready(onReady);
@@ -56,6 +62,19 @@ function addEmployeeInfo(event){
     let annualSalaryInputValue = $('#annual-salary-input').val();
     console.log('annualSalaryInputValue is:', annualSalaryInputValue);
 
+        // if(annualSalaryInputValue == NaN || ''){
+        //     console.log('Annual Salary must be a Number!:', annualSalaryInputValue);
+        //     // $('#first-name-input').val('');
+        //     // $('#last-name-input').val('');
+        //     // $('#id-input').val('');
+        //     // $('#title-input').val('');
+        //     // $('#annual-salary-input').val('');
+        //     return;
+        // }
+        // else {
+        //     console.log('Annual Salary is a Number:', annualSalaryInputValue);
+        // }
+
     // Append employee info into table
     $('#table-body').append(`
         <tr>
@@ -69,37 +88,31 @@ function addEmployeeInfo(event){
     `)
 
 
-
     // Using the stored information, calculate monthly costs 
-    // and append this to the to DOM. If the total monthly cost exceeds $20,000, 
-    // add a red background color to the total monthly cost.
+    // and append this to the to DOM.
     console.log('value to add to salary-monthly-total is:', `${annualSalaryInputValue}`/12)
-
-    // console.log('salaryMonthlyTotal before is: ', salaryMonthlyTotal)
-    // salaryMonthlyTotal = Number($('#salary-monthly-total').text() + `${annualSalaryInputValue}`/12);
-    // console.log('salaryMonthlyTotal after is: ', salaryMonthlyTotal)
-    
-    // $('#salary-monthly-total').val('');
-    // $('#salary-monthly-total').append(salaryMonthlyTotal);
-
     console.log('salaryMonthlyTotal before is: ', salaryMonthlyTotal)
-    salaryMonthlyTotal = salaryMonthlyTotal + `${annualSalaryInputValue}`/12;
-    // Number($('#salary-monthly-total').text() + `${annualSalaryInputValue}`/12);
+
+    salaryMonthlyTotal += `${annualSalaryInputValue}`/12;
     console.log('salaryMonthlyTotal after is: ', salaryMonthlyTotal)
+
+    // format salaryMonthlyTotal
+    // let formattedSalaryMonthlyTotal =  new Intl.NumberFormat('en-US', {
+    //     minimumFractionDigits: 2,
+    //     maximumFractionDigits: 2,
+    //     style: 'currency',
+    //     currency: 'USD',
+    // });
+    // console.log('formattedSalaryMonthlyTotal is: ', formattedSalaryMonthlyTotal);
+
+    // Append salary-monthly-total
     $('#salary-monthly-total').text(salaryMonthlyTotal)
 
-    if(salaryMonthlyTotal > 20000){
+    // If the total monthly cost exceeds $20,000, add a red background color 
+    // to the total monthly cost.
+    if(salaryMonthlyTotal > 20000.00){
         $('#salary-monthly-total').css('background-color', 'red')
     }
-
-
-
-    // $('#salary-monthly-total').append(function(salaryMonthlyTotal){
-    //     salaryMonthlyTotal++
-    //     return salaryMonthlyTotal;
-    //   });
-    // console.log('salaryMonthlyTotal after function is: ', salaryMonthlyTotal)
-
 
     // clear form
     $('#first-name-input').val('');
@@ -112,8 +125,6 @@ function addEmployeeInfo(event){
 }
 
 
-
-
 // Create a delete button that removes an employee from the DOM. For Base mode, 
 // it does **not** need to remove that Employee's salary from the reported total.
 function deleteEmployeeInfo(){
@@ -122,12 +133,3 @@ function deleteEmployeeInfo(){
     $(this).parent().parent().remove();
 }
 
-
-// Using the stored information, calculate monthly costs 
-// and append this to the to DOM. If the total monthly cost exceeds $20,000, 
-// add a red background color to the total monthly cost.
-function totalMonthlyCost(){
-    console.log('in totalMonthlyCost');
-
-
-}
